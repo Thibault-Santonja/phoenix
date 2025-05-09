b46b4a9b06f4fa8897a1aa1eefbcad75e17bb7da
Thibault Santonja <thibault.santonja@gmail.com>
2025-05-09 16:37:35 +0200

    Install Credo, Sobelow & mix_audit
    Useful libs for linting and auditing security issues.
    Lint code

--- mix.exs
+++ mix.exs
@@ -62,7 +62,11 @@ defmodule Portfolio.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      # For CI
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
+      {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false}
     ]
   end
 
