fdb0aeef7e81df9feebd8b55e813f923d54ebd23
Thibault Santonja <thibault.santonja@gmail.com>
2025-05-13 19:21:19 +0200

    Add app version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Portfolio.MixProject do
   def project do
     [
       app: :portfolio,
-      version: "0.1.0",
+      version: "0.1.12",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
