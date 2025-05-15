c91cb5caa74c06e931dc7c0fc0766730b580325d
Thibault Santonja <thibault.santonja@gmail.com>
2025-05-15 22:15:11 +0200

    Add missing moduledoc for `PortfolioWeb.Plugs.SetLocale`

--- lib/portfolio_web/plugs/set_locale.ex
+++ lib/portfolio_web/plugs/set_locale.ex
@@ -1,4 +1,11 @@
 defmodule PortfolioWeb.Plugs.SetLocale do
+  @moduledoc """
+    Plug responsible for defining the user's locale for the web application.
+
+    This plug checks whether a cookie named `"locale"` is present in the HTTP request. If this cookie contains a supported language (defined in `Gettext.known_locales/1`), this is used to set the active locale via `Gettext.put_locale/2`. The locale is also stored in the session (`:locale`).
+
+    If no valid locale is found, the default value `"fr"` is used.
+  """
   import Plug.Conn
   @behaviour Plug
   @supported_locales Gettext.known_locales(PortfolioWeb.Gettext)
