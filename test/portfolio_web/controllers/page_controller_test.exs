088869dc16fc233691b291d913fd9310286ef54b
Thibault Santonja <thibault.santonja@gmail.com>
2025-05-15 23:06:54 +0200

    Add tests & fix some pages

--- test/portfolio_web/controllers/page_controller_test.exs
+++ test/portfolio_web/controllers/page_controller_test.exs
@@ -1,8 +1,15 @@
 defmodule PortfolioWeb.PageControllerTest do
   use PortfolioWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  test "GET /amvcc", %{conn: conn} do
+    page = "amvcc"
+    conn = get(conn, ~p"/" <> page)
+    assert html_response(conn, 302) =~ "<a href=\"http://#{page}.www.example.com:80\">"
+  end
+
+  test "GET /photo", %{conn: conn} do
+    page = "photo"
+    conn = get(conn, ~p"/" <> page)
+    assert html_response(conn, 302) =~ "<a href=\"http://#{page}.www.example.com:80\">"
   end
 end
