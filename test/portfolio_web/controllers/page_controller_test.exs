2aada9e34e02b5b9c852e6451f95c8124bd9dd1f
Thibault Santonja <thibault.santonja@gmail.com>
2025-05-16 00:07:18 +0200

    Remove subdomain (#3)

--- test/portfolio_web/controllers/page_controller_test.exs
+++ test/portfolio_web/controllers/page_controller_test.exs
@@ -1,15 +1,3 @@
 defmodule PortfolioWeb.PageControllerTest do
   use PortfolioWeb.ConnCase
-
-  test "GET /amvcc", %{conn: conn} do
-    page = "amvcc"
-    conn = get(conn, ~p"/" <> page)
-    assert html_response(conn, 302) =~ "<a href=\"http://#{page}.www.example.com:80\">"
-  end
-
-  test "GET /photo", %{conn: conn} do
-    page = "photo"
-    conn = get(conn, ~p"/" <> page)
-    assert html_response(conn, 302) =~ "<a href=\"http://#{page}.www.example.com:80\">"
-  end
 end
--- test/portfolio_web/live/amvcc/index_live_test.exs
+++ test/portfolio_web/live/amvcc/index_live_test.exs
@@ -2,35 +2,19 @@ defmodule PortfolioWeb.Amvcc.IndexLiveTest do
   use PortfolioWeb.ConnCase
   import Phoenix.LiveViewTest
 
-  defp open_subdomain(%{conn: conn}) do
-    {:error, {:redirect, %{to: subdomain}}} = live(conn, ~p"/amvcc")
-
-    %{conn: conn, subdomain: subdomain}
-  end
-
   describe "AMVCC index" do
-    setup [:open_subdomain]
-
-    test "/ path render default page", %{conn: conn, subdomain: subdomain} do
-      {:ok, _index_live, html} = live(conn, subdomain <> "/")
+    test "/amvcc path render default page", %{conn: conn} do
+      {:ok, _index_live, html} = live(conn, "/amvcc")
       assert html =~ "La Seigneurie de Coucy"
     end
-  end
 
-  describe "Clothes page" do
-    setup [:open_subdomain]
-
-    test "/ path render default page", %{conn: conn, subdomain: subdomain} do
-      {:ok, _index_live, html} = live(conn, subdomain <> "/vetements")
+    test "/amvcc/vetements path render clothes page", %{conn: conn} do
+      {:ok, _index_live, html} = live(conn, "/amvcc/vetements")
       assert html =~ "habit au milieu du XIV<sup>e</sup> siècle"
     end
-  end
 
-  describe "Shoes page" do
-    setup [:open_subdomain]
-
-    test "/ path render default page", %{conn: conn, subdomain: subdomain} do
-      {:ok, _index_live, html} = live(conn, subdomain <> "/chaussures")
+    test "/amvcc/chaussures path render shoes page", %{conn: conn} do
+      {:ok, _index_live, html} = live(conn, "/amvcc/chaussures")
       assert html =~ "Les chaussures au Moyen Âge (XIV<sup>e</sup> siècle)"
     end
   end
