97d211a6d1769f618fb00709f58dd8a8aae74c46
Thibault Santonja <thibault.santonja@gmail.com>
2025-05-16 19:16:28 +0200

    Add an article about the current CI

---
+++ lib/portfolio_web/live/tech_live/blog/ci.ex
@@ -0,0 +1,20 @@
+defmodule PortfolioWeb.TechLive.Blog.Ci do
+  use PortfolioWeb, :live_view
+
+  @impl true
+  def mount(_, session, socket) do
+    Gettext.put_locale(PortfolioWeb.Gettext, session["locale"])
+
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, gettext("The tech blog") <> " • " <> gettext("CI"))
+  end
+end
