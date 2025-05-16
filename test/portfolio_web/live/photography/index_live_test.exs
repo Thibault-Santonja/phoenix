4eeaccde0d532d7c4a9a0e6282a13cdde8568943
Thibault Santonja <thibault.santonja@gmail.com>
2025-05-16 09:20:44 +0200

    Rollback to subdomains

--- test/portfolio_web/live/photography/index_live_test.exs
+++ test/portfolio_web/live/photography/index_live_test.exs
@@ -2,9 +2,17 @@ defmodule PortfolioWeb.Photography.IndexLiveTest do
   use PortfolioWeb.ConnCase
   import Phoenix.LiveViewTest
 
+  defp open_subdomain(%{conn: conn}) do
+    {:error, {:redirect, %{to: subdomain}}} = live(conn, ~p"/photo")
+
+    %{conn: conn, subdomain: subdomain}
+  end
+
   describe "Photography index" do
-    test "/ path render default page", %{conn: conn} do
-      {:ok, _index_live, html} = live(conn, "/photo")
+    setup [:open_subdomain]
+
+    test "/ path render default page", %{conn: conn, subdomain: subdomain} do
+      {:ok, _index_live, html} = live(conn, subdomain <> "/")
       assert html =~ "Thibault San Photographie"
     end
   end
